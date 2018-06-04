82dd9de594672fcdb91a7630b294037bae96c260
Tomas Baca <tomas.baca@fel.cvut.cz>
2018-06-04 17:43:55 +0200

    Revert "updated epigen"
    This reverts commit 67df92cd037c505b32ac9e3edbe76a8f9a2ea98d, reversing
    changes made to 91a716f6199ad3d9f24cb2a381e8722ebbf5f571.

--- example/deploy_configs.sh
+++ example/deploy_configs.sh
@@ -1,6 +1,4 @@
-# - make the script run in bash/zsh while having its dotfile sourced
-# - this is important when there are variables exported, which might
-#   be used by this script
+# make the script run in bash/zsh while having the dotfile sourced
 PNAME=$( ps -p "$$" -o comm= )
 SNAME=$( echo "$SHELL" | grep -Eo '[^/]+/?$' )
 if [ "$PNAME" != "$SNAME" ]; then
